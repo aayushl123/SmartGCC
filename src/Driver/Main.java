2fc4ef1ba8581df641de0da8875953a5aabf0c4d
Sagar Bhatia <sagar101283cse@gmail.com.com>
2020-03-25 02:29:09 -0400

    Created the welcome. Will finish the branch after we finalize this screen.

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -1,18 +1,20 @@
 package Driver;
 
+import View.WelcomeScene;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        WelcomeScene.setWelcomeScene();
         Parent root = FXMLLoader.load(getClass().getResource("../View/sample.fxml"));
         primaryStage.setTitle("SmartGCC");
-        primaryStage.setScene(new Scene(root, 1264, 775));
+//        primaryStage.setScene(new Scene(root, 1264, 775));
+        primaryStage.setScene(WelcomeScene.getWelcomeScene());
         primaryStage.show();
     }
 
---
+++ src/View/WelcomeScene.java
@@ -0,0 +1,60 @@
+package View;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+import java.io.FileInputStream;
+
+public class WelcomeScene {
+    private static Scene welcomeScene;
+    private static ImageView imageView;
+
+    public static void setWelcomeScene(){
+        Label playerLabel = new Label("Please select the type of user and press OK:");
+        playerLabel.setFont(new Font("Arial", 24));
+        ToggleGroup radioGroup = new ToggleGroup();
+
+        Button submitButton = new Button("OK");
+        RadioButton radioButton1 = new RadioButton("1. Novice user");
+        RadioButton radioButton2 = new RadioButton("2. Typical User");
+        RadioButton radioButton3 = new RadioButton("3. Expert User");
+
+        submitButton.setStyle("-fx-font-size:20px;");
+        radioButton1.setStyle("-fx-font-size:20px;");
+        radioButton2.setStyle("-fx-font-size:20px;");
+        radioButton3.setStyle("-fx-font-size:20px;");
+        radioButton1.setToggleGroup(radioGroup);
+        radioButton2.setToggleGroup(radioGroup);
+        radioButton3.setToggleGroup(radioGroup);
+
+        setImageView();
+        VBox vbox = new VBox(20, imageView, playerLabel,radioButton1, radioButton2, radioButton3, submitButton);
+        vbox.setAlignment(Pos.CENTER);
+        welcomeScene = new Scene(vbox, 1264, 775);
+    }
+
+    public static Scene getWelcomeScene(){
+        return welcomeScene;
+    }
+
+    public static ImageView setImageView(){
+        try {
+            Image image = new Image(new FileInputStream("src/Resources/WelcomeScreenImage.png"));
+            imageView = new ImageView(image);
+            imageView.setX(50);
+            imageView.setY(25);
+            imageView.setPreserveRatio(true);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return imageView;
+    }
+}
