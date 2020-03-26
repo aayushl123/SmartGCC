4c7b8e22c2561305db3e9755552df385f1ae0684
aayushl123 <ayus.lami7@gmail.com>
2020-03-25 21:42:28 -0400

    Backend initialization (Install GCC before using)

---
+++ src/Backend/CompileRun.java
@@ -0,0 +1,35 @@
+package Backend;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class CompileRun {
+
+    public static void main(String[] args) {
+        String command1 = "g++ " + "c:\\Users\\ayusl\\Desktop\\test.cpp " + "-o " + "test";
+        String command2 = "c:\\Users\\ayusl\\Desktop\\test.exe";
+        System.out.println(command1);
+        executeCommand(command1);
+        executeCommand(command2);
+    }
+
+    public static void executeCommand(String command) {
+        String line;
+        try {
+            ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", command);
+            builder.redirectErrorStream(true);
+            Process p = builder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception = " + e.getMessage());
+        }
+
+    }
+
+
+
+}
