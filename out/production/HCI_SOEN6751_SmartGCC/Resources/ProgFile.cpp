a4b71523f4867a021fbf213137ea76784845f31b
rohitsharma <rohit.sharma06596@gmail.com>
2020-04-02 21:03:43 -0400

    Edits to make Linking process clear

--- out/production/HCI_SOEN6751_SmartGCC/Resources/ProgFile.cpp
+++ out/production/HCI_SOEN6751_SmartGCC/Resources/ProgFile.cpp
@@ -14,8 +14,11 @@ int main() {
     std::cout << "main() says Hello World!\n"<<
     "The encryption standard is " << ENCRPTION_STANDARD<<"\n";
     int val = takeVal();
+    std::cout<<"The value to be encoded is "<<val<<"\n";
     double encVal = encrypt(val);
+    std::cout<<"The encoded value is "<<encVal<<"\n";
     int decVal = decrypt(encVal);
+    std::cout<<"The decoded value is "<<decVal<<"\n";
     if(decVal == val){
         std::cout<<"Both encoding and decoding were successful\n";
     }
